d3e459b8118e4c274443f795a625e19e7bd2a33a
Gavin Canon-Phratsachack <37220586+gncnpk@users.noreply.github.com>
2025-06-11 17:08:04 -0500

    Create waze-discuss-profile-settings-plus.user.js

---
+++ waze-discuss-profile-settings-plus.user.js
@@ -0,0 +1,99 @@
+// ==UserScript==
+// @name         Waze Discuss Profile Settings+
+// @namespace    https://github.com/WazeDev/waze-discuss-profile-settings-plus
+// @version      0.0.1
+// @description  Add quality-of-life features to the profile settings page on Waze Discuss.
+// @author       Gavin Canon-Phratsachack (https://github.com/gncnpk)
+// @match        https://www.waze.com/discuss/u/*/preferences/profile
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=waze.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    let bioSection;
+    let signatureSection;
+    let signatureTextArea;
+    let signatureButtonBar;
+    document.onreadystatechange = function () {
+  if (document.readyState == "complete") {
+      bioSection = document.getElementsByClassName("controls bio-composer input-xxlarge")[0];
+      bioSection.style.width = "70%";
+        signatureSection= document.getElementsByClassName("ember-view user-custom-preferences-outlet signature-preferences")[0].parentElement;
+      signatureSection.style.width = "200%";
+        signatureTextArea = document.getElementsByClassName("ember-text-area ember-view d-editor-input")[1];
+      signatureTextArea.style.height = "80%";
+      signatureButtonBar = document.getElementsByClassName("d-editor-button-bar")[1];
+      addWMESignatureButtons()
+  }
+}
+    function generateButton(element, title, icon) {
+        let id = title.replace(" ", "-").toLowerCase();
+        element.innerHTML += `<button class="btn no-text btn-icon" id="waze-discuss-${id}-button" tabindex="-1" title="Insert ${title}" type="button">
+<img src="${icon}" style="
+    width: 1em;
+    height: 1em;
+">      <span aria-hidden="true">
+          &ZeroWidthSpace;
+        </span>
+    </button>`;
+        document.getElementById(`waze-discuss-${id}-button`).addEventListener("click", function (e) {appendIconToSignature(icon)});
+    }
+    async function addWMESignatureButtons() {
+        const stats = await getWMEStats();
+        let icon;
+        let title;
+        // Area Manager badge
+        if(stats.isAreaManager){
+            icon = "https://s.waze.tools/am.png"
+            title = "AM Badge"
+            generateButton(signatureButtonBar, title, icon)
+        }
+        // Total Edits badge
+        if(stats.totalEdits >= 10000 && stats.totalEdits < 50000) {
+            icon = "https://s.waze.tools/s0010.png";
+            title = "10k Total Edits";
+            generateButton(signatureButtonBar, title, icon)
+        } else if (stats.totalEdits >= 50000 && stats.totalEdits < 100000) {
+            icon = "https://s.waze.tools/s0050.png";
+            title = "50k Total Edits";
+            generateButton(signatureButtonBar, title, icon)
+        } else if (stats.totalEdits >= 100000 && stats.totalEdits < 300000) {
+            icon = "https://s.waze.tools/s0100.png";
+            title = "100k Total Edits";
+            generateButton(signatureButtonBar, title, icon)
+        } else if (stats.totalEdits >= 300000 && stats.totalEdits < 500000) {
+            icon = "https://s.waze.tools/s0300.png";
+            title = "300k Total Edits";
+            generateButton(signatureButtonBar, title, icon)
+        } else if (stats.totalEdits >= 500000 && stats.totalEdits < 1000000) {
+            icon = "https://s.waze.tools/s0500.png";
+            title = "500k Total Edits";
+            generateButton(signatureButtonBar, title, icon)
+        } else if (stats.totalEdits >= 1000000 && stats.totalEdits < 1500000) {
+            icon = "https://s.waze.tools/s1000.png";
+            title = "1M Total Edits";
+            generateButton(signatureButtonBar, title, icon)
+        } else if (stats.totalEdits >= 1500000 && stats.totalEdits < 2000000) {
+            icon = "https://s.waze.tools/s1500.png";
+            title = "1.5M Total Edits";
+            generateButton(signatureButtonBar, title, icon)
+        } else if (stats.totalEdits >= 2000000) {
+            icon = "https://s.waze.tools/s2000.png";
+            title = "2M Total Edits";
+            generateButton(signatureButtonBar, title, icon)
+        }
+        // Rank badge
+        icon = `https://s.waze.tools/c${stats.rank+1}.png`
+        title = `Rank ${stats.rank+1} Badge`
+        generateButton(signatureButtonBar, title, icon)
+    }
+    function appendIconToSignature(url) {
+        signatureTextArea.value += `[img]${url}[/img]`
+    }
+    async function getWMEStats() {
+        const res = await fetch("https://www.waze.com/Descartes/app/Session?language=en-US");
+        let jsonData = await res.json();
+        return jsonData;
+    }
+})();
