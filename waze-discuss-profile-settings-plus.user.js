558d97b636415d8d201950c1fbe905f092adb169
Gavin Canon-Phratsachack <37220586+gncnpk@users.noreply.github.com>
2025-06-11 17:55:04 -0500

    Update waze-discuss-profile-settings-plus.user.js

--- waze-discuss-profile-settings-plus.user.js
+++ waze-discuss-profile-settings-plus.user.js
@@ -1,13 +1,15 @@
 // ==UserScript==
 // @name         Waze Discuss Profile Settings+
 // @namespace    https://github.com/WazeDev/waze-discuss-profile-settings-plus
-// @version      0.0.2
+// @version      0.0.3
 // @description  Add quality-of-life features to the profile settings page on Waze Discuss.
 // @author       Gavin Canon-Phratsachack (https://github.com/gncnpk)
 // @match        https://www.waze.com/discuss/u/*/preferences/profile
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=waze.com
 // @grant        none
 // @license      MIT
+// @downloadURL https://update.greasyfork.org/scripts/539152/Waze%20Discuss%20Profile%20Settings%2B.user.js
+// @updateURL https://update.greasyfork.org/scripts/539152/Waze%20Discuss%20Profile%20Settings%2B.meta.js
 // ==/UserScript==
 
 (function() {
@@ -16,81 +18,88 @@
     let signatureSection;
     let signatureTextArea;
     let signatureButtonBar;
-    document.onreadystatechange = function () {
-  if (document.readyState == "complete") {
-      bioSection = document.getElementsByClassName("controls bio-composer input-xxlarge")[0];
-      bioSection.style.width = "70%";
-        signatureSection= document.getElementsByClassName("ember-view user-custom-preferences-outlet signature-preferences")[0].parentElement;
-      signatureSection.style.width = "200%";
-        signatureTextArea = document.getElementsByClassName("ember-text-area ember-view d-editor-input")[1];
-      signatureTextArea.style.height = "80%";
-      signatureButtonBar = document.getElementsByClassName("d-editor-button-bar")[1];
-      console.log("Resized signature and biography elements!")
-      addWMESignatureButtons()
-  }
-}
+    document.onreadystatechange = function() {
+        if (document.readyState == "complete") {
+            bioSection = document.getElementsByClassName("controls bio-composer input-xxlarge")[0];
+            bioSection.style.width = "70%";
+            signatureSection = document.getElementsByClassName("ember-view user-custom-preferences-outlet signature-preferences")[0].parentElement;
+            signatureSection.style.width = "200%";
+            signatureTextArea = document.getElementsByClassName("ember-text-area ember-view d-editor-input")[1];
+            signatureTextArea.style.height = "80%";
+            signatureButtonBar = document.getElementsByClassName("d-editor-button-bar")[1];
+            console.log("Resized signature and biography elements!")
+            addWMESignatureButtons()
+        }
+    }
+
     function generateButton(element, title, icon) {
-        let id = title.replace(" ", "-").toLowerCase();
-        element.innerHTML += `<button class="btn no-text btn-icon" id="waze-discuss-${id}-button" tabindex="-1" title="Insert ${title}" type="button">
-<img src="${icon}" style="
-    width: 1em;
-    height: 1em;
-">      <span aria-hidden="true">
-          &ZeroWidthSpace;
-        </span>
-    </button>`;
-        document.getElementById(`waze-discuss-${id}-button`).addEventListener("click", function (e) {appendIconToSignature(icon)});
+        let id = `waze-discuss-${title.replace(" ", "-").toLowerCase()}-button`;
+        let button = document.createElement("button");
+        let img = document.createElement("img");
+        img.src = icon;
+        img.style.width = "1em";
+        img.style.height = "1em";
+        button.append(img)
+        button.className = "btn no-text btn-icon";
+        button.id = id;
+        button.title = `Insert ${title}`;
+        button.type = "Button";
+        button.addEventListener("click", function(e) {
+            appendIconToSignature(icon)
+        });
+        element.append(button);
     }
     async function addWMESignatureButtons() {
         const stats = await getWMEStats();
         let icon;
         let title;
         // Area Manager badge
-        if(stats.isAreaManager){
+        if (stats.isAreaManager) {
             icon = "https://s.waze.tools/am.png"
             title = "AM Badge"
-            generateButton(signatureButtonBar, title, icon)
+            await generateButton(signatureButtonBar, title, icon)
         }
         // Total Edits badge
-        if(stats.totalEdits >= 10000 && stats.totalEdits < 50000) {
+        if (stats.totalEdits >= 10000 && stats.totalEdits < 50000) {
             icon = "https://s.waze.tools/s0010.png";
             title = "10k Total Edits";
-            generateButton(signatureButtonBar, title, icon)
+            await generateButton(signatureButtonBar, title, icon)
         } else if (stats.totalEdits >= 50000 && stats.totalEdits < 100000) {
             icon = "https://s.waze.tools/s0050.png";
             title = "50k Total Edits";
-            generateButton(signatureButtonBar, title, icon)
+            await generateButton(signatureButtonBar, title, icon)
         } else if (stats.totalEdits >= 100000 && stats.totalEdits < 300000) {
             icon = "https://s.waze.tools/s0100.png";
             title = "100k Total Edits";
-            generateButton(signatureButtonBar, title, icon)
+            await generateButton(signatureButtonBar, title, icon)
         } else if (stats.totalEdits >= 300000 && stats.totalEdits < 500000) {
             icon = "https://s.waze.tools/s0300.png";
             title = "300k Total Edits";
-            generateButton(signatureButtonBar, title, icon)
+            await generateButton(signatureButtonBar, title, icon)
         } else if (stats.totalEdits >= 500000 && stats.totalEdits < 1000000) {
             icon = "https://s.waze.tools/s0500.png";
             title = "500k Total Edits";
-            generateButton(signatureButtonBar, title, icon)
+            await generateButton(signatureButtonBar, title, icon)
         } else if (stats.totalEdits >= 1000000 && stats.totalEdits < 1500000) {
             icon = "https://s.waze.tools/s1000.png";
             title = "1M Total Edits";
-            generateButton(signatureButtonBar, title, icon)
+            await generateButton(signatureButtonBar, title, icon)
         } else if (stats.totalEdits >= 1500000 && stats.totalEdits < 2000000) {
             icon = "https://s.waze.tools/s1500.png";
             title = "1.5M Total Edits";
-            generateButton(signatureButtonBar, title, icon)
+            await generateButton(signatureButtonBar, title, icon)
         } else if (stats.totalEdits >= 2000000) {
             icon = "https://s.waze.tools/s2000.png";
             title = "2M Total Edits";
-            generateButton(signatureButtonBar, title, icon)
+            await generateButton(signatureButtonBar, title, icon)
         }
         // Rank badge
         icon = `https://s.waze.tools/c${stats.rank+1}.png`
         title = `Rank ${stats.rank+1} Badge`
-        generateButton(signatureButtonBar, title, icon)
+        await generateButton(signatureButtonBar, title, icon)
         console.log("Added buttons for badges to signature editor!")
     }
+
     function appendIconToSignature(url) {
         signatureTextArea.value += `[img]${url}[/img]`
     }
