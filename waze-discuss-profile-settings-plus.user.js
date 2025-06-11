77ed3a8d8b9e9447f78ee2ae9dd860c73e9a0dad
Gavin Canon-Phratsachack <37220586+gncnpk@users.noreply.github.com>
2025-06-11 17:12:03 -0500

    Update waze-discuss-profile-settings-plus.user.js

--- waze-discuss-profile-settings-plus.user.js
+++ waze-discuss-profile-settings-plus.user.js
@@ -7,6 +7,7 @@
 // @match        https://www.waze.com/discuss/u/*/preferences/profile
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=waze.com
 // @grant        none
+// @license      MIT
 // ==/UserScript==
 
 (function() {
